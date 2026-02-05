3e5b6df6f182ead9c8c3c6173bf907c7bfcc9526
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-02-05 01:22:53 +0000

    Criar endpoint `/status` #21

--- modules/IsNull.js
+++
@@ -1,5 +0,0 @@
-function IsNull(arg){
-  return arg === null || arg === undefined
-}
-
-exports.IsNull = IsNull;
--- tests/IsTrue.test.js
+++
@@ -1,6 +0,0 @@
-const IsNullFunc = require("../modules/IsNull.js");
-
-test("Algo com valor não é nulo", () =>{
-  expect(IsNullFunc.IsNull(1)).toBe(false);
-  expect(IsNullFunc.IsNull(null)).toBe(true);
-});
