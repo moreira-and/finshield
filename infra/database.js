d412ccb51f6a412a962678b3184eb2a3ddbe6b08
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-02-09 22:38:00 +0000

    add the getSSLConfig function to implement the CA reference in the .env file. #29

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLConfig(),
   });
 
   let result;
@@ -22,6 +22,16 @@ async function query(queryObject) {
   }
 
   return result;
+}
+
+function getSSLConfig() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
