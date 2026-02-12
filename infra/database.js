929e0a931581aad458acbdd419036c8994064f8c
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-02-12 23:58:28 +0000

    Refact database module #23

--- infra/database.js
+++ infra/database.js
@@ -31,9 +31,7 @@ function getSSLConfig() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
-export default {
-  query: query,
-};
+export default { query };
