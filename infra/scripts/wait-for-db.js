8fdafeb80c53f4d925140f00d7de6c9c8d5431dc
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-02-18 22:34:54 +0000

    adds wait-for-db.js script

---
+++ infra/scripts/wait-for-db.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkDatabaseConnection() {
+  exec("docker exec database-dev pg_isready --host localhost", handleReturn);
+}
+
+function handleReturn(error, stdout, stderr) {
+  if (stdout.search("accepting connections") === -1) {
+    process.stdout.write(".");
+    setTimeout(checkDatabaseConnection, 1000); // Tenta novamente em 5 segundos
+    return;
+  }
+
+  console.log("\n🟢 Banco de dados pronto e aceitando conexões!");
+  return;
+}
+
+process.stdout.write("\n⏳ Aguardando o banco de dados ficar pronto.");
+checkDatabaseConnection();
