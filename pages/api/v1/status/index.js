53deab699a8507a182cc280686efed015f092c14
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-03-06 10:07:02 +0000

    refactor: add next-connect to `api/v1/status` and use the router to handle `OnError` and `OnNoMatch`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,31 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors.js";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
 
-async function status(request, response) {
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
+
+function onErrorHandler(error, request, response) {
+  const publicErrorObject = new InternalServerError({
+    cause: error,
+  });
+  console.info("Error dentro do catch do next-connect.");
+  console.error("Error fetching /api/v1/status:", publicErrorObject);
+  return response.status(publicErrorObject.status_code).json(publicErrorObject);
+}
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  return response.status(publicErrorObject.status_code).json(publicErrorObject);
+}
+
+async function getHandler(request, response) {
   const queryDbVersion = "SHOW server_version;";
   const queryActiveConn = {
     text: `
@@ -11,39 +35,29 @@ async function status(request, response) {
         AND datname = $1;`,
     values: [process.env.POSTGRES_DB],
   };
+
   const queryMaxConn = "SHOW max_connections;";
+  const currentTime = new Date().toISOString();
 
-  try {
-    const currentTime = new Date().toISOString();
+  const resultDbVersion = await database.query(queryDbVersion);
+  const resultActiveConnections = await database.query(queryActiveConn);
+  const resultMaxConnections = await database.query(queryMaxConn);
 
-    const resultDbVersion = await database.query(queryDbVersion);
-    const resultActiveConnections = await database.query(queryActiveConn);
-    const resultMaxConnections = await database.query(queryMaxConn);
+  const versionValue = resultDbVersion.rows[0].server_version;
+  const activeConnectionsValue =
+    resultActiveConnections.rows[0].active_connections;
+  const maxConnectionsValue = parseInt(
+    resultMaxConnections.rows[0].max_connections,
+  );
 
-    const versionValue = resultDbVersion.rows[0].server_version;
-    const activeConnectionsValue =
-      resultActiveConnections.rows[0].active_connections;
-    const maxConnectionsValue = parseInt(
-      resultMaxConnections.rows[0].max_connections,
-    );
-    return response.status(200).json({
-      updated_at: currentTime,
-      dependences: {
-        database: {
-          version: versionValue,
-          active_connections: activeConnectionsValue,
-          max_connections: maxConnectionsValue,
-        },
+  return response.status(200).json({
+    updated_at: currentTime,
+    dependences: {
+      database: {
+        version: versionValue,
+        active_connections: activeConnectionsValue,
+        max_connections: maxConnectionsValue,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-    console.info("Error dentro do controller de status");
-    console.error("Error fetching status:", publicErrorObject);
-    return response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
