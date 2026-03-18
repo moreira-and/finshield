e99c0a20141460ba23dd8f6a70167f4bbc17fa12
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-03-18 10:09:53 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -68,6 +68,33 @@ async function create(userInputValues) {
   }
 }
 
-const user = { create };
+async function findOneByUsername(username) {
+  const user = await runSelectQuery(username);
+
+  return user;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+      SELECT *
+      FROM users
+      WHERE LOWER(username) = LOWER($1)
+      LIMIT 1
+      ;`,
+      values: [username],
+    });
+
+    if (result.rowCount == 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return result.rows[0];
+  }
+}
+
+const user = { create, findOneByUsername };
 
 export default user;
---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,96 @@
+import orchestrator from "tests/orchestrator";
+import { version as uuidVersion } from "uuid";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("GET /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With exact case match", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "MesmoCase",
+          email: "mesmo.case@gmail.com",
+          password: "123senha",
+        }),
+      });
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/MesmoCase",
+      );
+
+      expect(response2.status).toBe(200);
+
+      const responseBody2 = await response2.json();
+      expect(responseBody2).toEqual({
+        id: responseBody2.id,
+        username: "MesmoCase",
+        email: "mesmo.case@gmail.com",
+        password: "123senha",
+        created_at: responseBody2.created_at,
+        updated_at: responseBody2.updated_at,
+      });
+      expect(uuidVersion(responseBody2.id)).toBe(4);
+      expect(Date.parse(responseBody2.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody2.updated_at)).not.toBeNaN();
+    });
+
+    test("With case mismatch", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "MissMatch",
+          email: "miss.match@gmail.com",
+          password: "123senha",
+        }),
+      });
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/missmatch",
+      );
+
+      expect(response2.status).toBe(200);
+
+      const responseBody2 = await response2.json();
+      expect(responseBody2).toEqual({
+        id: responseBody2.id,
+        username: "MissMatch",
+        email: "miss.match@gmail.com",
+        password: "123senha",
+        created_at: responseBody2.created_at,
+        updated_at: responseBody2.updated_at,
+      });
+      expect(uuidVersion(responseBody2.id)).toBe(4);
+      expect(Date.parse(responseBody2.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody2.updated_at)).not.toBeNaN();
+    });
+
+    test("With nonexistent username", async () => {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/nonexistent",
+      );
+
+      expect(response.status).toBe(404);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    });
+  });
+});
