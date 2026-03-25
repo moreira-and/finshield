1d3d356700886e3970241615ebb2b26afb34770c
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-03-25 09:47:32 +0000

    feat(auth): add pepper to password before hashing

--- models/password.js
+++ models/password.js
@@ -3,15 +3,23 @@ import bcryptjs from "bcryptjs";
 async function hash(password) {
   console.log(process.env);
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  const pepperedPassword = addPepperToPassword(password);
+
+  return await bcryptjs.hash(pepperedPassword, rounds);
 }
 
 function getNumberOfRounds() {
   return process.NODE_ENV === "production" ? 14 : 1;
 }
 
+function addPepperToPassword(password) {
+  const pepper = process.env.PEPPER;
+  return password + pepper;
+}
+
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  const pepperedPassword = addPepperToPassword(providedPassword);
+  return await bcryptjs.compare(pepperedPassword, storedPassword);
 }
 
 const password = {
